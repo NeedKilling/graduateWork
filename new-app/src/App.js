3edf764fb48bdf6221934a4ef82623dfc0f5fb73
Makar <vsyakofazno@gmail.com>
2023-04-17 23:18:01 +0300

    add Routes in app file and route.path Pages

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import $ from 'jquery'
-import {Header} from './component';
-import {Footer} from './component';
+import {Header, Footer} from './component';
 
+import {Routes,Route, Link} from 'react-router-dom'
+import { Home, Profile, Catalog, Popular, Subscription} from './Pages';
 
 function App() {
   return (
 <div className="wrap">
 <Header/>
+
+
+  <Routes>
+      <Route  path="/" element={<Home/>}/>
+      <Route  path="/Catalog" element={<Catalog/>}/>
+      <Route  path="/Profile" element={<Profile/>}/>
+      <Route  path="/Popular" element={<Popular/>}/>
+      <Route  path="/Subscription" element={<Subscription/>}/>
+  </Routes>
+
+
 <Footer/>
 </div>
   )
