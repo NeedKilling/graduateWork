634c8782a8e509f99690bbdef0b2dd576c18a9bf
Makar <vsyakofazno@gmail.com>
2023-04-17 23:18:46 +0300

    add route in react

--- new-app/src/index.js
+++ new-app/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './sass/style.sass';
 import App from './App';
-
+import { BrowserRouter as Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+<React.StrictMode>
+  <Route>
     <App />
-  </React.StrictMode>
+  </Route>  
+</React.StrictMode>
 );
 
