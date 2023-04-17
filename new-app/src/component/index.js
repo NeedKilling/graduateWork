46ec735d7fa91354f49db80a326caade2d6f13d2
Makar <vsyakofazno@gmail.com>
2023-04-17 23:05:39 +0300

    export component

--- new-app/src/component/index.js
+++ new-app/src/component/index.js
@@ -1,6 +1,8 @@
-import Categorias from "./Categorias";
-import Footer from "./Footer";
-import Header from "./Header/Header";
-import ProfileDrop from "./Header/ProfileDrop";
-
-export{Categorias, Footer, Header, ProfileDrop}
+export {default as Categorias} from "./Categorias";
+export {default as Footer} from "./Footer";
+export {default as Header} from "./Header/Header";
+export {default as ProfileDrop} from "./Header/ProfileDrop";
+export {default as SortBar} from "./SortBar";
+export {default as Sort} from "./Sort";
+export {default as SliderBig} from "./Slider/SliderBig";
+export {default as SliderMin} from "./Slider/SliderMin";
