c3b3b4b3c1a25167473ef838d14083a86bea0740
Makar <vsyakofazno@gmail.com>
2023-04-20 17:19:16 +0300

    fetch geting in json files and

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import $ from 'jquery'
 import {Header, Footer} from './component';
 
-import {Routes,Route, Link} from 'react-router-dom'
-import { Home, Profile, Catalog, Popular, Subscription} from './Pages';
+import {Routes,Route} from 'react-router-dom'
+import { Home, Profile, Catalog, Popular, Subscription} from './Pages'; 
 
 function App() {
+const [bookBlock,setBookBlock] = React.useState([])
+
+React.useEffect(()=>{
+  fetch("http://localhost:3000/db.json").then((response)=> response.json()).then(json=>{
+    setBookBlock(json.book)
+  });
+}, []);
+
+console.log(bookBlock)
+
+
+
   return (
 <div className="wrap">
 <Header/>
 
 
   <Routes>
-      <Route  path="/" element={<Home/>}/>
-      <Route  path="/Catalog" element={<Catalog/>}/>
-      <Route  path="/Profile" element={<Profile/>}/>
-      <Route  path="/Popular" element={<Popular/>}/>
-      <Route  path="/Subscription" element={<Subscription/>}/>
+      <Route  path="/" Component={()=><Home items = {bookBlock}/>}/> {/*<Component items={bookBlock}/>*/}
+      <Route  path="/Catalog" Component={()=><Catalog items = {bookBlock}/>} exact/>
+      <Route  path="/Profile" Component={Profile}/>
+      <Route  path="/Popular" Component={Popular}/>
+      <Route  path="/Subscription" Component={Subscription}/>
   </Routes>
 
 
