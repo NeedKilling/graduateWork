394ed32e78ab8cf3b9540855e30fc9d900121ff3
Makar <vsyakofazno@gmail.com>
2023-04-25 00:16:42 +0300

    create store and add provider in react-redux

--- new-app/src/index.js
+++ new-app/src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './sass/style.sass';
-import App from './App';
+
 import { BrowserRouter as Route } from 'react-router-dom';
 
+import {createStore} from 'redux';
+import store from './redux/store';
+import { Provider } from 'react-redux';
+
+import './sass/style.sass';
+import App from './App';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
 <React.StrictMode>
   <Route>
+    <Provider store = {store}>
     <App />
+    </Provider>
   </Route>  
 </React.StrictMode>
 );
