42ad39dde85d99e7c37cc5badfb7c10f908853f7
Makar <vsyakofazno@gmail.com>
2023-04-30 03:42:53 +0300

    BookPage

---
+++ new-app/src/Pages/BookPage.jsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom';
+import { useEffect,useState } from 'react';
+import { setBooks } from '../redux/actions/books';
+
+import axios from 'axios'
+import { useDispatch } from 'react-redux';
+
+
+import star from "../assets/icons/star.svg"
+import drop from "../assets/icons/drop.svg"
+
+function BookPage() {
+    const items = useSelector((state) => state.Books.items);
+    //  console.log(items[0].name)
+    const {id} = useParams()
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        axios.get(`http://localhost:3000/db.json/${id}`).then(({data}) =>{
+        dispatch(setBooks(data.book))
+        },[id])})
+    console.log(items)
+  return (
+
+    <div>
+       <div className="Page">    
+    <section className="bookPage">
+        <div className="background" style ={{backgroundImage: `url(${items[id].backgroundImage})`}}></div>    
+            <div className="container">
+                <div className="wrapper">
+                    <div className="bookPage_right">
+                        <div className="bookPage_img"><img src={items[id].imageUrl} alt=""></img></div>
+                        <div className="bookPage_link continue">Читать</div>
+                        <div className="bookPage_dropdown">
+                            <div className="mark">Добавить в<img src={drop} alt=""></img> </div>
+                            <ul className="dropdown_ul">
+                                <li className="dropdown_ul__item">Читаю</li>
+                                <li className="dropdown_ul__item">В планах</li>
+                                <li className="dropdown_ul__item">Прочитано</li>
+                                <li className="dropdown_ul__item">Любимое</li>
+                                <li className="dropdown_ul__item">Брошено</li>
+                            </ul>
+                        </div>
+                        
+                    </div>
+                    <div className="bookPage_left">
+                        <div className="head">
+                            <div className="title">
+                                <div className="name">{items[id].name}/</div>
+                                <div className="subName">{items[id].subName}</div>
+                            </div>
+                            <div className="rating">
+                                <img src={star} alt=""></img>
+                                <div className="number">5</div>
+                            </div>
+                        </div>
+                        <div className="content">
+                            <div className="wrapper">
+                                <div className="block">
+                                    <div className="content_item">Год выпуска</div>
+                                    <div className="content_item">Страна</div>
+                                    <div className="content_item">Жанр</div>
+                                    <div className="content_item">Автор</div>
+                                    <div className="content_item">Художник</div>
+                                    <div className="content_item">Издательсво</div>
+                                </div>
+                            <div className="block">
+                                    <div className="content_item">{items[id].datarelize}</div>
+                                    <div className="content_item">{items[id].country}</div>
+                                    <div className="content_item">{items[id].genre}</div>
+                                    <div className="content_item">{items[id].author}</div>
+                                    <div className="content_item">{items[id].artist}</div>
+                                    <div className="content_item">{items[id].publisher}</div>
+                            </div>
+                            </div>
+                            <div className="description">
+                                {items[id].description}
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+        </div>
+    </section>
+</div>
+        
+    </div>
+  )
+}
+
+export default BookPage
