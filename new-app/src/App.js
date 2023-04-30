9a3715311299b17ec4f6ee583770467da0b0600b
Makar <vsyakofazno@gmail.com>
2023-04-30 03:48:27 +0300

    new route<db.json/:id> and dispatch axios request

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,44 +1,60 @@
-import React, { Component } from 'react';
+import React from 'react';
 import axios from 'axios'
+import {Routes,Route} from 'react-router-dom'
+import {useSelector,useDispatch} from 'react-redux'
+import store from './redux/store';
 
 import {Header, Footer} from './component';
+import { Home, Profile, Catalog, Popular, Subscription,BookPage} from './Pages'; 
+import {setBooks} from './redux/actions/books'
 
-import {Routes,Route} from 'react-router-dom'
-import { Home, Profile, Catalog, Popular, Subscription} from './Pages'; 
 
-function App() {
-const [bookBlock,setBookBlock] = React.useState([])
-
-React.useEffect(()=>{
-  axios.get("http://localhost:3000/db.json").then(({data}) =>{
-    setBookBlock(data.book)
+function App(){
+  const dispatch = useDispatch();
+ 
+  React.useEffect(()=>{
+    axios.get("http://localhost:3000/db.json").then(({data}) =>{
+    dispatch(setBooks(data.book))
   });
-  // fetch("http://localhost:3000/db.json").then((response)=> response.json()).then(json=>{
-  //   setBookBlock(json.book)
-  // });
-}, []);
+  },[]);
 
-console.log(bookBlock)
-
-
-
-  return (
-<div className="wrap">
-<Header/>
-
-
-  <Routes>
-      <Route  path="/" Component={()=><Home items = {bookBlock}/>}/> {/*<Component items={bookBlock}/>*/}
-      <Route  path="/Catalog" Component={()=><Catalog items = {bookBlock}/>} exact/>
-      <Route  path="/Profile" Component={Profile}/>
-      <Route  path="/Popular" Component={Popular}/>
-      <Route  path="/Subscription" Component={Subscription}/>
-  </Routes>
-
-
-<Footer/>
-</div>
-  )
+  return(
+    
+    <div className="wrap">
+      <Header/>
+      
+      
+        <Routes>
+            <Route  path="/" Component={Home}/> {/*<Component items={bookBlock}/>*/}
+            <Route  path="/Catalog" Component={Catalog} exact/>
+            <Route  path="/Profile" Component={Profile}/>
+            <Route  path="/Popular" Component={Popular}/>
+            <Route  path="/Subscription" Component={Subscription}/>
+            {/* <Route path='/BookPage/:id' Component={BookPage}/> */}
+            <Route path='/db.json/:id' Component={BookPage}/>
+            
+        </Routes>
+      
+      
+      
+      <Footer/>
+      </div>   
+  );
 };
 
+
 export default App;
+
+// const mapStateToProps = (state) =>{
+//   return{
+//     items: state.Books.items,
+//     filtres: state.filtres
+//   }
+// }
+
+// const mapDispatchToProps ={
+// setBooksAction  
+
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
