6f9b1c91a719b3f9f884b79fef63c1346a981a11
Makar <vsyakofazno@gmail.com>
2023-05-09 00:59:08 +0300

    React memo Categorias

--- new-app/src/component/Categorias.jsx
+++ new-app/src/component/Categorias.jsx
@@ -1,38 +1,40 @@
 import React,{ useState } from 'react'
 
 
-function Categorias ({items,onClickItem}){
-  const [activeState,setActiveState] = useState(null);
+const Categorias = React.memo(
+  function Categorias ({items,onClickItem}){
+    const [activeState,setActiveState] = useState(null);
+    
+    const selectState = (index) => {
+      setActiveState(index);
+      onClickItem(index)
+    }
   
-  const selectState = (index) => {
-    setActiveState(index);
-    onClickItem(index)
+      return (
+         
+           <ul class="profile_list">
+  
+              <li onClick = {() => selectState(null)} 
+              className={activeState === null ? 'item item_active' : 'item'}>Все</li>
+              {
+                items && items.map((name,index)=><li 
+  
+                onClick = {()=> selectState(index)}
+  
+                key={`${name}_${index}`} 
+  
+                className={activeState === index ? 'item item_active': 'item'}>{name}
+  
+                </li>)
+              }
+              <div>
+        
+      </div>
+          </ul>
+         
+        )
+    
   }
-
-    return (
-       
-         <ul class="profile_list">
-
-            <li onClick = {() => selectState(null)} 
-            className={activeState === null ? 'item item_active' : 'item'}>Все</li>
-            {
-              items && items.map((name,index)=><li 
-
-              onClick = {()=> selectState(index)}
-
-              key={`${name}_${index}`} 
-
-              className={activeState === index ? 'item item_active': 'item'}>{name}
-
-              </li>)
-            }
-            <div>
-      
-    </div>
-        </ul>
-       
-      )
-  
-}
+)
 
 export default Categorias;
