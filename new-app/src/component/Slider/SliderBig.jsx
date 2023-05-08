e89353c6b02ec682fc954a1a2ab63a21a201bb44
Makar <vsyakofazno@gmail.com>
2023-05-09 01:11:23 +0300

    new Url

--- new-app/src/component/Slider/SliderBig.jsx
+++ new-app/src/component/Slider/SliderBig.jsx
@@ -34,7 +34,7 @@ export default function SliderBig ({items}) {
         <Slider {...settings}>
             {
                 items && items.map(obj => (
-                   <Link key ={obj.id} to = {`/db.json/${obj.id}`}>
+                   <Link key ={obj.id} to = {`/Book/${obj.id}`}>
                         <a href="#" className="slider_book">
                             <div className="slider_book_img"><img src={obj.imageUrl}  alt="book"></img></div>
                             <div className="slider_book_name">{obj.name}</div>
