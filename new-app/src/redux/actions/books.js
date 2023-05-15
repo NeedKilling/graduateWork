f868f4579e55b262ef628fef7634a474bf203eac
Makar <vsyakofazno@gmail.com>
2023-05-15 06:43:42 +0300

    add dynamic query based on props by sortBy and categorias and add set_loading

--- new-app/src/redux/actions/books.js
+++ new-app/src/redux/actions/books.js
@@ -1,4 +1,31 @@
+import axios from 'axios'
 
+export const setLoaded = (payload) => ({
+    type: 'SET_LOADING',
+    payload,
+})
+
+export const fetchBooksCatalog = (sortBy) => (dispatch)=>{
+    console.log(sortBy)
+    dispatch({
+        type: 'SET_LOADING',
+        payload: false,
+    })
+    axios.get(`http://localhost:3001/book?_sort=${sortBy.type},vievs&_order=${sortBy.order}`).then(({data}) =>{
+    dispatch(setBooks(data));
+});
+};
+
+export const fetchBooksProfile = (categorias) => (dispatch)=>{
+    console.log(categorias)
+    dispatch({
+        type: 'SET_LOADING',
+        payload: false,
+    })
+    axios.get(`http://localhost:3001/book?${categorias !== null ? `category=${categorias}` : ''}`).then(({data}) =>{
+    dispatch(setBooks(data));
+});
+};
 
 export const setBooks= (items)=>({
     type: "SET_BOOKS",
