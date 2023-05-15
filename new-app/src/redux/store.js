032c05643549b65996ba057c75d9f185dd5fb1a8
Makar <vsyakofazno@gmail.com>
2023-05-15 09:19:29 +0300

    add compose and composeEnhancer

--- new-app/src/redux/store.js
+++ new-app/src/redux/store.js
@@ -1,8 +1,9 @@
-import { createStore } from 'redux'
+import { createStore,compose,applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
-let store = createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-window.store = store;
+let store = createStore(rootReducer,composeEnhancer(applyMiddleware(thunk)))
 
 export default store;
