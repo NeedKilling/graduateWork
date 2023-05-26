4bbf4e98be4178f57faba21dd192285bb12619b3
Makar <vsyakofazno@gmail.com>
2023-05-27 00:07:00 +0300

    actions add books profile

--- new-app/src/redux/actions/profieList.js
+++ new-app/src/redux/actions/profieList.js
@@ -1,14 +1,18 @@
 import axios from 'axios'
 
-export const fetchBooks = () => (dispatch)=>{
-    axios.get("http://localhost:3001/book").then(({data}) =>{
-    dispatch(setProfileCat(data));
-});
-};
+// export const fetchBooks = () => (dispatch)=>{
+//     axios.get("http://localhost:3001/book").then(({data}) =>{
+//     dispatch(setProfileCat(data));
+// });
+// };
 
 
-const setProfileCat = (items)=>({
-    type: "SET_PROFILE",
-    payload : items,
+// const setProfileCat = (items)=>({
+//     type: "SET_PROFILE",
+//     payload : items,
 
-});
+// });
+export const addBooksProfile = (bookOBJ) =>({
+    type: "ADD_BOOKS_PROFILE",
+    payload: bookOBJ
+})
