2d1d1f428d28506a4c88e8ac61589481405dc81e
Makar <vsyakofazno@gmail.com>
2023-05-27 00:05:27 +0300

    actions genre sort and country sort

--- new-app/src/redux/actions/filtres.js
+++ new-app/src/redux/actions/filtres.js
@@ -5,6 +5,17 @@ export const setSortBy = ({type,order})=>({
     payload : {type,order},
 
 });
+export const setSortByBar = (name)=>({
+    type: "SET_SORT_BY_BAR",
+    payload : name,
+
+});
+export const setSortByBarGenre = (name)=>({
+    type: "SET_SORT_BY_BAR_GENRE",
+    payload : name,
+
+});
+
 export const setCategorias = (value)=>({
     type: "SET_CATEGORIAS",
     payload : value,
