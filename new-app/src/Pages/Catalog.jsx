13ff008a3a7c571ffa85e83350f02c9e35020be3
Makar <vsyakofazno@gmail.com>
2023-05-27 00:01:24 +0300

    dispatch bar name and dispatch bar genre

--- new-app/src/Pages/Catalog.jsx
+++ new-app/src/Pages/Catalog.jsx
@@ -6,29 +6,37 @@ import BookCatalog from '../component/BookBlock/BookCatalog';
 import { useSelector,useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchBooks, fetchBooksCatalog } from '../redux/actions/books';
-import {setSortBy} from "../redux/actions/filtres"
+import {setSortBy, setSortByBar,setSortByBarGenre} from "../redux/actions/filtres"
 
 function Catalog (){
     const dispatch = useDispatch();
     const items = useSelector((state) => state.Books.items)
     const isLoaded = useSelector((state) => state.Books.isLoaded)
-    const {sortBy} = useSelector(({filtres}) => filtres);    
-
+    const {sortBy,sortByBar,sortByBarGenre} = useSelector(({filtres}) => filtres);     
 
     React.useEffect(()=>{
-        dispatch(fetchBooksCatalog(sortBy))
-    },[sortBy]);
+        dispatch(fetchBooksCatalog(sortBy,sortByBar,sortByBarGenre))
+    },[sortBy,sortByBar,sortByBarGenre]);
 
     const onClickSortType = React.useCallback((type) => {
         dispatch(setSortBy(type));
     },[]);
 
+    const onClickSortBarName = React.useCallback((name)=>{
+        dispatch(setSortByBar(name))
+    },[])
+    
+    const onClickSortBarGenre = React.useCallback((name)=>{
+        dispatch(setSortByBarGenre(name))
+    },[])
+
     const SortName = [
         {name: "рейтингу",type:'rating',order:'desc'},
         {name: "алфавиту",type: "name", order:'asc'},
         {name: "дате релиза(↗)",type: "datarelize",order:'desc'},
-        // {name: "дате релиза(↘)",type: "datarelize",order:'asc'}
     ]
+    const SortCountry = ["Япония","Корея","Китай"]
+    const SortGenre = ["фентези","комедия","романтика","боевик","драма","триллер","фантастика","ужасы"]
       
         return(
             <div>
@@ -41,14 +49,21 @@ function Catalog (){
             </div>
             <div className="content"> 
                {
-               isLoaded ? items.map(obj => (
-                    <Link key = {obj.id} to = {`/Book/${obj.id}`} {...obj} >
-                    <BookCatalog key = {obj.id} isLoading = {true} {...obj}/></Link> 
+               isLoaded ? items.map((obj,index) => (
+                    <Link key = {`${obj.name}_${index}`} to = {`/Book/${obj.id}`} {...obj} >
+                    <BookCatalog key = {`${obj.name}_${index}`} isLoading = {true} {...obj}/></Link> 
                 )) : Array(15).fill(<LoadingCatalog/>)
                } 
               
             </div>
-            <SortBar/>
+            <SortBar 
+                activeSort = {sortByBar} 
+                country = {SortCountry} 
+                onClickSortBarName = {onClickSortBarName}
+                activeSortGenre = {sortByBarGenre}
+                genre = {SortGenre}
+                onClickSortBarGenre = {onClickSortBarGenre} 
+            />
         </div>
     </div>
 </div>            
