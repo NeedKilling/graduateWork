475ff4aa38474699a340186031f5141be9d2fce1
Makar <vsyakofazno@gmail.com>
2023-05-27 00:09:28 +0300

    add books profile reducers very hard

--- new-app/src/redux/reducers/profileList.js
+++ new-app/src/redux/reducers/profileList.js
@@ -1,18 +1,45 @@
 
 
+// const initialState = {
+//     items : [],
+//     isLoaded : false
+//  }
+ 
+//  const profileList = (state = initialState,action) => {
+//      if(action.type ==="SET_PROFILE"){
+//          return{
+//              ...state,
+//              items: action.payload,
+//          };
+//      }
+//      return state;
+//  }
+ 
+//  export default profileList; 
+
 const initialState = {
-    items : [],
-    isLoaded : false
+    items : {},
+    totalCount: 0
  }
  
- const profileList = (state = initialState,action) => {
-     if(action.type ==="SET_PROFILE"){
-         return{
-             ...state,
-             items: action.payload,
-         };
+ const Profile = (state = initialState,action) => {
+    switch(action.type){
+         case "ADD_BOOKS_PROFILE":{
+            const newItems = {
+                    ...state.items,   /// возьмет старое                  
+                    [action.payload.id]: !state.items[action.payload.id] 
+                    ? [action.payload] 
+                    : [...state.items[action.payload.id],action.payload]
+            }
+             return{
+                 ...state,         // вверни весь старое состояние (обЪект) 
+                 items: newItems,
+                 totalCount: [].concat.apply([], Object.values(newItems)).length
+             };
+            }
+         default:
+             return state
      }
-     return state;
  }
  
- export default profileList; 
+ export default Profile; 
