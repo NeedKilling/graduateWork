66242885d05d84f22576198470cdb221a1bc41cb
Makar <vsyakofazno@gmail.com>
2023-05-26 23:30:29 +0300

    refactor imageUrl to imageurl subName to subname

--- new-app/src/component/BookBlock/BookCatalog.jsx
+++ new-app/src/component/BookBlock/BookCatalog.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 
 
-function BookCatalog({name,subName,imageUrl}) {
+function BookCatalog({name,subname,imageurl}) {
 
   return (
     <div className="book">
-        <img src={imageUrl} alt=""></img>
+        <img src={imageurl} alt=""></img>
         <div className="book_title">
             <div className="book_name">{name}</div>
-            <div className="book_subname">{subName}</div>
+            <div className="book_subname">{subname}</div>
             <div className = "tooltipText">{name}</div>
         </div>
     </div>
