cae9ee4ea8737f481247ff180de3cf8510afa1cd
Makar <vsyakofazno@gmail.com>
2023-05-27 00:08:03 +0300

    add sort country and sort genre reducers

--- new-app/src/redux/reducers/filtres.js
+++ new-app/src/redux/reducers/filtres.js
@@ -4,7 +4,9 @@ const initialState = {
     sortBy : {
         type: "rating",
         order: 'desc',
-    }
+    },
+    sortByBar: null,
+    sortByBarGenre: null
 }
 
 const filtres = (state = initialState,action) => {
@@ -18,6 +20,18 @@ const filtres = (state = initialState,action) => {
         return{
             ...state,
             categorias: action.payload,
+        };
+    }
+    if(action.type ==="SET_SORT_BY_BAR"){
+        return{
+            ...state,
+            sortByBar: action.payload,
+        };
+    }
+    if(action.type ==="SET_SORT_BY_BAR_GENRE"){
+        return{
+            ...state,
+            sortByBarGenre: action.payload,
         };
     }
     return state;
