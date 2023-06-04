5a1781708b7c447144ee4c2797e2efb8af0e37ec
Makar <vsyakofazno@gmail.com>
2023-06-05 00:52:01 +0300

    added Link to BookPage

--- new-app/src/component/BookBlock/BookProfile.jsx
+++ new-app/src/component/BookBlock/BookProfile.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function BookProfile({name,subName,imageUrl}) {
+function BookProfile({id,name,subName,imageurl,type}) {
 
   return (
-    <div class="content_item">
-    <div class="img"><img src={imageUrl} alt="book"></img></div>
-     <div class="text">
-         <a href="#" class="name">{name}</a>
-         <div class="subName">{subName}</div>
-         <button class="continue">Продолжить</button>
+    <div className="content_item">
+    <Link to={`/Book/${id}`} className="img"><img src={imageurl} alt="book"></img></Link>
+     <div className="text">
+         <Link to={`/Book/${id}`} class="name">{name}</Link>
+         <div className="subName">{subName}</div>
+         <Link to = {`/Book/${id}/${name}`}><button className="continue">{type}</button></Link>
      </div>
  </div>
   )
