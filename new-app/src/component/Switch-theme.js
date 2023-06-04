526d578394d05fa4d55890ca9afea2ce76aaa4d8
Makar <vsyakofazno@gmail.com>
2023-06-05 01:02:23 +0300

    switch theme and save for local Storage

---
+++ new-app/src/component/Switch-theme.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const isDarkTheme = window?.matchMedia('(prefers-color-scheme: dark)').matchMedia
+const defaultTheme = isDarkTheme === 'dark' ? 'dark' : "light"
+export const useTheme=() =>{
+    const[theme,setTheme] = React.useState(localStorage.getItem('app-theme') || defaultTheme)
+
+    React.useEffect(()=>{
+        document.documentElement.setAttribute('data-theme',theme)
+        localStorage.setItem('app-theme',theme)
+    },[theme])
+    return{theme,setTheme}
+}
+
