47dfe3bab71d5908de5f724ba3a609ac743bc661
Makar <vsyakofazno@gmail.com>
2023-06-05 01:05:39 +0300

    .

--- new-app/src/component/Slider/SliderBig.jsx
+++ new-app/src/component/Slider/SliderBig.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Slider from "react-slick";
 import { Link } from "react-router-dom";
 
-
 export default function SliderBig ({items}) {
     const settings = {
     //   dots: true,
