e116a47b45b24a272b19e5c036067af939d3a99b
Makar <vsyakofazno@gmail.com>
2023-06-12 19:13:39 +0300

    Remove_Profile_Item action

--- new-app/src/redux/actions/profieList.js
+++ new-app/src/redux/actions/profieList.js
@@ -19,3 +19,8 @@ export const addBooksProfile = (bookOBJ) =>({
 export const ClearProfile = () =>({
     type: "CLEAR_PROFILE",
 })
+export const removeProfileItem = (id) =>({
+    type: "REMOVE_PROFILE_ITEM",
+    payload: id,
+})
+
