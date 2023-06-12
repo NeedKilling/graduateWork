98ea6ea1e3c1ba4b2270ac621765f15c1d566921
Makar <vsyakofazno@gmail.com>
2023-06-12 18:46:08 +0300

    added logOutUser, added searh filter

--- new-app/src/component/Header/Header.jsx
+++ new-app/src/component/Header/Header.jsx
@@ -1,18 +1,50 @@
-import React, { useState ,useRef} from 'react';
+import React, { useState ,useRef, useContext} from 'react';
 import ProfileDrop from './ProfileDrop'
 import { useSelector } from 'react-redux';
-import {Link} from 'react-router-dom'
-
+import {Link,useNavigate} from 'react-router-dom'
+import {AiOutlineUser} from "react-icons/ai"
+import {ImExit} from "react-icons/im"
 
 import Logo from "../../assets/img/Logo.svg"
 import search from "../../assets/icons/search.svg"
 import profileAvatar from "../../assets/img/avatar_none.png"
 import moon from "../../assets/icons/moon.png"
 import sun from "../../assets/icons/sun.png"
+import {CiUser} from "react-icons/ci"
 
 import { useTheme } from '../../component/Switch-theme'; 
+import { AuthContext } from '../../hoc/AuthProvider';
 
 function Header(){
+    const navigate = useNavigate()
+
+    const {user,setUser} = useContext(AuthContext)
+
+    const logOutUser = () => {
+        setUser({
+            email:''
+        })
+        localStorage.removeItem('user')
+        navigate('/')
+    }
+
+///////////////////////
+    const DropRef = React.useRef(); 
+    const [dropActive,setDropActive] = React.useState(false)
+    const taggleDrop = () => {
+        setDropActive(!dropActive)
+    }
+    const handleOutClick = (e)=>{
+        if (e.target !== DropRef.current){
+            setDropActive(false)
+        }      
+        }
+
+    React.useEffect(() => {
+        document.addEventListener('click' , handleOutClick);
+    },[]);
+
+
     const {theme,setTheme} = useTheme()
 
     const switchRefDote = useRef()
@@ -39,13 +71,35 @@ function Header(){
         setTheme('light')
     }
 
-const [dropActive,setDropActive] = useState(true)
+
    
 const {totalCount} = useSelector(({Profile}) => ({
     totalCount: Profile.totalCount,
 }))
     const items = useSelector(({Profile}) => Profile.items);
     const total = Object.keys(items)
+
+    const itemsCatalog = useSelector((state) => state.Books.items)
+
+    const [valueSearh,setValueSearh] = React.useState('')
+
+    const filtresBooks = itemsCatalog.filter(book => {
+       return book.name.toLowerCase().includes(valueSearh.toLowerCase()) || book.subname.toLowerCase().includes(valueSearh.toLowerCase())
+   })
+
+   const reset = () =>{
+        setValueSearh('')
+   }
+
+
+//////////////////////////
+    
+
+   
+
+   
+   
+
 return(
     <div>
 <header className="js-header">
@@ -59,8 +113,31 @@ return(
             </ul>
             <div className="searh">
                 <form className="searh_form">
+
                     <div className='submit' type="submit"><img src={search} alt="search"></img></div>
-                    <input type="text" placeholder="Поиск по названию..."></input>
+
+                    <input value = {valueSearh} onChange = {(e)=>setValueSearh(e.target.value)} type="text" placeholder="Поиск по названию..."></input>
+
+                    <div onClick={reset} className="clear"></div>
+
+                        <ul className="autocomplete">
+                            {
+                                filtresBooks.map((obj,index)=>(
+                                    valueSearh  
+                                        ? <Link to = {`/Book/${obj.id}`}>
+                                            <li key={`${obj.name}_${index}`} className='autocomplete_item'>
+                                                <img className="autocomplete_item_img"  src={obj.imageurl} alt="image"/>
+                                                <div className="autocomplete_item_text">
+                                                    <div className="name">{obj.name}</div>
+                                                    <div className="subname">{obj.subname}</div>
+                                                </div>
+                                            </li>
+                                        </Link>
+                                        : null
+                                ))
+                            }
+                        </ul>
+                    
                 </form>
             </div>
                
@@ -73,13 +150,31 @@ return(
                         <span ref = {switchRefDote}  onClick = {handleSwitch} className='dote'></span>
                 </div>
                     
-                    <Link to={'/Profile'}>
-                        <div onClick={() => setDropActive(!dropActive)} className='dropdown-profile'>
-                                <img src={profileAvatar} alt="avatar_none"></img>
-                                <div className='dropdown-profile_counter'>{total.length}</div>
-                            {/* <ProfileDrop active={dropActive} setActive={setDropActive}/> */}
-                        </div>   
-                    </Link>    
+                    {
+                    user.email ? 
+                        
+                            <div  className='dropdown-profile'>
+                                    <img ref = {DropRef} onClick={taggleDrop} src={profileAvatar} alt="avatar_none"></img>
+                                    <div className='dropdown-profile_counter'>{total.length}</div>
+                                        {
+                                            dropActive &&
+                                                <div  className="dropdown-content">
+                                                    <Link to="/Profile"><div className="dropdown-content__item"><AiOutlineUser/>Профиль</div></Link>
+                                                    {/* <div  className="dropdown-content__item"><img src={bookmark} alt=""></img>Закладки</div> */}
+                                                    <div className="dropdown-content__item line"></div>
+                                                    <div onClick = {logOutUser} className="dropdown-content__item btn">{<ImExit/>}<span>Выйти</span></div>
+                                                </div>
+                                        }
+                            </div>   
+                        
+                    :
+                        <Link to = {'/LogIn'} className='login'>
+                            <div className='login_img'><CiUser/></div>
+                            <div className='login_text'>Войти</div>
+                        </Link>
+                    }
+                     
+                      
             </div>     
             
             
