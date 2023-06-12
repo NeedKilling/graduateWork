1f284fd44de8fecb9baa2e20a4bfbbfbaefacf76
Makar <vsyakofazno@gmail.com>
2023-06-12 18:54:25 +0300

    modified

--- new-app/src/component/Switch-theme.js
+++ new-app/src/component/Switch-theme.js
@@ -3,7 +3,7 @@ import React from "react"
 const isDarkTheme = window?.matchMedia('(prefers-color-scheme: dark)').matchMedia
 const defaultTheme = isDarkTheme === 'dark' ? 'dark' : "light"
 export const useTheme=() =>{
-    const[theme,setTheme] = React.useState(localStorage.getItem('app-theme') || defaultTheme)
+    const[theme,setTheme] = React.useState(localStorage.getItem('app-theme') || defaultTheme)   
 
     React.useEffect(()=>{
         document.documentElement.setAttribute('data-theme',theme)
