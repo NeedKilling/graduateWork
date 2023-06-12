6680c666cd2b9cf1767fa9a301a1ee33dbf6f8ab
Makar <vsyakofazno@gmail.com>
2023-06-12 18:50:25 +0300

    redirect to registration

--- new-app/src/component/BookMarkInPage.jsx
+++ new-app/src/component/BookMarkInPage.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
-import ClassNames from "classnames"
-import drop from "../assets/icons/drop.svg"
 import { useSelector } from 'react-redux';
-import { object } from 'prop-types';
-
+import { Link } from 'react-router-dom';
+import { MdOutlineKeyboardDoubleArrowUp} from "react-icons/md"
 
 const BookMarkInPage = React.memo(
-    function BookMarkInPage({id,name,subName,imageurl,onClickAddBook,onAddedCategorias,activeCategorias}) {
+    function BookMarkInPage({user,id,name,subName,imageurl,onClickAddBook,onAddedCategorias,activeCategorias}) {
     
         const items = useSelector(({Profile}) => Profile.items); 
-         
+
         const category = ["Читаю","В планах","Прочитано","Любимое","Брошено"];
     
         const [visibleDrop , setVisibleDrop] = React.useState(false)
         const DropRef = React.useRef();
+        const textRef = React.useRef();
         let [activeDrop, setActiveDrop] = React.useState()
 
         let activeNameDrop = category[activeDrop] // предмет[0]
        
             const taggleDrop = () => {
+
                 setVisibleDrop(!visibleDrop)
             }
             const handleOutClick = (e)=>{
-                if (e.target !== DropRef.current){
+                if (e.target !== textRef.current){
                     setVisibleDrop(false)
                 }      
             }
@@ -34,8 +34,10 @@ const BookMarkInPage = React.memo(
     
         React.useEffect(() => {
             document.addEventListener('click' , handleOutClick);
+            
         },[]);
     
+        
         const switchActive = (index) =>{
             onAddedCategorias(index)
             setActiveDrop(index)
@@ -50,42 +52,64 @@ const BookMarkInPage = React.memo(
         }
     
       return (
-        <div className="bookPage_dropdown">
-            <div ref = {DropRef} onClick={taggleDrop} 
-                className={
-                    ClassNames(
-                        activeNameDrop === undefined ? "mark":"",
-                        activeNameDrop === "Читаю" ? "mark read":"",
-                        activeNameDrop === "В планах" ? "mark inPlan":"",
-                        activeNameDrop === "Прочитано" ? "mark viewed":"",
-                        activeNameDrop === "Любимое" ? "mark favorite":"",
-                        activeNameDrop === "Брошено" ? "mark abandoned":"",
-                    )}>
-    
-                <div className='text'>
-                    {activeNameDrop === undefined ? "Добавить в": activeNameDrop} 
-                </div>
-                <img className={visibleDrop?"img active":"img"} src={drop} alt=""></img>
-            </div>
-            {
-                visibleDrop && (<ul className='dropdown_ul'> {/*скрытие и появление выпадаюшего меню*/}
-                    {                    
-                        category.map((name,index) =>
-                        <li key={`${name}_${index}`}
-                        onClick={()=>switchActive(index)}
-                        className={activeDrop === index ? "dropdown_ul__item dropdown_ul__item_active" : "dropdown_ul__item"}>
-                        {name}
-                        </li>)
-                    }
+<>       
+        {
+            user ? 
+            <>
+                <div className="bookPage_dropdown">
+                    
+                    <div className='mark'>
+                        {/* // className={
+                        //     ClassNames(
+                        //         activeNameDrop === undefined ? "mark":"",
+                        //         activeNameDrop === "Читаю" ? "mark read":"",
+                        //         activeNameDrop === "В планах" ? "mark inPlan":"",
+                        //         activeNameDrop === "Прочитано" ? "mark viewed":"",
+                        //         activeNameDrop === "Любимое" ? "mark favorite":"",
+                        //         activeNameDrop === "Брошено" ? "mark abandoned":"",
+                        //     )} */}
+                        <div onClick={taggleDrop} ref = {textRef} className='text'>
+                            {activeNameDrop === undefined ? "Добавить в": activeNameDrop} 
+                        </div>
+                        <MdOutlineKeyboardDoubleArrowUp className={visibleDrop?"img active":"img"}/>
+                    </div>
                     {
-                        activeNameDrop && (<li onClick={deleteName}className='dropdown_ul__item dropdown_ul__item_delete'>Удалить из списка</li>) // удаление при выборе
+                        visibleDrop && (<ul className='dropdown_ul'> {/*скрытие и появление выпадаюшего меню*/}
+                            {                    
+                                category.map((name,index) =>
+                                <li key={`${name}_${index}`}
+                                onClick={()=>switchActive(index)}
+                                className={activeDrop === index ? "dropdown_ul__item dropdown_ul__item_active" : "dropdown_ul__item"}>
+                                {name}
+                                </li>)
+                            }
+                            {/* {
+                                activeNameDrop && (<li onClick={deleteName}className='dropdown_ul__item dropdown_ul__item_delete'>Удалить из списка</li>) // удаление при выборе
+                            } */}
+                        </ul>)
                     }
-                </ul>)
-            }
-                
-        </div>
+                        
+                </div>
+            </> : 
+            
+            <>
+                <Link to = {"/LogIn"} className="bookPage_dropdown">
+                    
+                    <div className='mark'>
+                        <div className='text'>
+                           Добавить в
+                        </div>
+                    </div>
+                </Link>
+            </>
+        }
+            
+            
+        
+</>       
       )
     }
+
 )
 
 export default BookMarkInPage
